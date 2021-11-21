e74248751540737af886f5afddcaa3a6c6259e07
yjseo <YJ0722>
2021-11-21 15:25:29 +0900

    [#3] Design Pattern(Adapter) - yeonju

---
+++ Design_Pattern/Structural/AdapterPattern/Example/AudioPlayer.java
@@ -0,0 +1,18 @@
+public class AudioPlayer implements MediaPlayer {
+
+  MediaAdapter mediaAdapter;
+
+  @Override
+  public void play(String audioType, String fileName) {
+
+    if(audioType.equalsIgnoreCase("mp3")){
+      System.out.println("Playing mp3 file. Name: " + fileName);
+    }
+
+    // 'vlc' 나 'mp4' 포맷은 MediaAdapter 로 처리한다.
+    else if(audioType.equalsIgnoreCase("vlc") || audioType.equalsIgnoreCase("mp4")){
+      mediaAdapter = new MediaAdapter(audioType);
+      mediaAdapter.play(audioType, fileName);
+    }
+  }
+}
---
+++ Design_Pattern/Structural/AdapterPattern/Example/MediaAdapter.java
@@ -0,0 +1,27 @@
+public class MediaAdapter implements MediaPlayer {
+
+    AdvancedMediaPlayer advancedMusicPlayer;
+  
+    public MediaAdapter(String audioType){
+  
+      // 생성 시, vlc 인지 mp4 인지 구분.
+      if(audioType.equalsIgnoreCase("vlc") ){
+        advancedMusicPlayer = new VlcPlayer();            
+  
+      }else if (audioType.equalsIgnoreCase("mp4")){
+        advancedMusicPlayer = new Mp4Player();
+      }    
+    }
+  
+    @Override
+    public void play(String audioType, String fileName) {
+  
+      // play 호출 시, 구체적인 각 플레이어의 메쏘드를 호출.
+      if(audioType.equalsIgnoreCase("vlc")){
+        advancedMusicPlayer.playVlc(fileName);
+      }
+      else if(audioType.equalsIgnoreCase("mp4")){
+        advancedMusicPlayer.playMp4(fileName);
+      }
+    }
+  }
